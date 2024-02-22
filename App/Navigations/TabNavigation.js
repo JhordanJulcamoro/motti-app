e1282bdc1a96ec76226224f4a73ffe20eef0aa0c
jhordanjulcamoro <jhordan.julcamoro@ms4m.com>
2024-02-22 05:53:12 -0500

    Navigation icons

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -5,17 +5,57 @@ import Home from "../Screens/Home";
 import Fav from "../Screens/Fav";
 import Search from "../Screens/Search";
 import Profile from "../Screens/Profile";
+import { Ionicons } from "react-native-vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function TabNavigation() {
   const Tab = createBottomTabNavigator();
   return (
-    <Tab.Navigator screenOptions={{
-        headerShown:false
-        }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Fav" component={Fav} />
-      <Tab.Screen name="Profile" component={Profile} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Fav"
+        component={Fav}
+        options={{
+          tabBarLabel: "Favorite",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user-circle-o" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
