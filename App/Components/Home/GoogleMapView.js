79559e040d6e29030e7626982d13a05fc981c3ca
jhordanjulcamoro <jhordan.julcamoro@ms4m.com>
2024-02-25 21:15:39 -0500

    Google marker in map

--- App/Components/Home/GoogleMapView.js
+++ App/Components/Home/GoogleMapView.js
@@ -1,24 +1,25 @@
-import { View, Text, Dimensions } from "react-native";
+import { View, Text, Dimensions, Image } from "react-native";
 import React, { useEffect, useState, useContext } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import * as Location from "expo-location";
 import { UserLocationContext } from "../../Context/UserLocationContext";
 
 export default function GoogleMapView() {
-  const [mapRegion, setmapRegion] = useState([]);
+  const [mapRegion, setmapRegion] = useState(null);
 
   const { location, setLocation } = useContext(UserLocationContext);
 
-  useEffect(()=> {
-    if(location){
+  useEffect(() => {
+    if (location) {
       setmapRegion({
         latitude: location.coords.latitude,
-        longitude:  location.coords.longitude,
+        longitude: location.coords.longitude,
         latitudeDelta: 0.01,
         longitudeDelta: 0.0421,
-      })
+      });
+      console.log("mapRegion::",mapRegion)
     }
-  }, [])
+  }, [location]);
 
   return (
     <View style={{ marginTop: 5 }}>
@@ -32,7 +33,21 @@ export default function GoogleMapView() {
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         region={mapRegion}
-      />
+        customMapStyle={[]}
+        customMarker={<Image source={require("../../../assets/icon_motti.png")}/>}
+      >
+        {/* {mapRegion && (
+          <Marker
+          title="You are here"
+          description="This is your current location"
+          coordinate={{
+            latitude: mapRegion.latitude,
+            longitude: mapRegion.longitude,
+          }}
+          image={require("../../../assets/icon_motti.png")}
+          />
+        )} */}
+      </MapView>
     </View>
   );
 }
